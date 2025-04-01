07a88777fc2339f1d84188bad3cd98cd33dd4b72
Karol-OC <karol.oc2006@gmail.com>
2025-03-31 21:53:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+import colors from 'colors'
+
+console.log('texto azul' .blue)
+console.log('arriba los pumas' .underline.blue)
+console.log('arriba los pumas' .inverse.red)
+console.log('arriba los pumas' .rainbow)
